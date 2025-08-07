dcf70b094103cb2fe0effd47e4336c5b5710f25a
Abhay-Pratap-Singh-7 <abhaypratapsingh2993@gmail.com>
2025-08-07 22:41:34 +0530

    Add optimal solution for a question

--- Array_Questions/number_shownup_once.cpp
+++ Array_Questions/number_shownup_once.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // Brute TC = O(n^2)
-int getFreqOne(vector<int> &arr){
+int getFreqOne_M1(vector<int> &arr){
     int flag;
     for ( int i = 0; i < arr.size(); i++ ){
         flag = 0;
@@ -18,13 +18,25 @@ int getFreqOne(vector<int> &arr){
     return -1;
 }
 
+// optimal TC = O(n)
+int getFreqOne_M2(vector<int> &arr){
+    int xOR = 0;
+    for ( int i = 0; i < arr.size(); i++ ){
+        xOR ^= arr[i];
+    }
+    if ( xOR == 0 ) return -1;
+    return xOR;
+}
+
 int main(){
 
     cout << endl;
 
     vector<int> arr = {1,1,2,2,3,3,4,4};
 
-    cout << getFreqOne(arr) << endl;
+    cout << getFreqOne_M1(arr) << endl;
+    cout << getFreqOne_M2(arr) << endl;
+
     cout << endl;
 
 }
