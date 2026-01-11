3224dd8644b8d90239c9a0a34224ab6bb1ceb31e
Abhay-Pratap-Singh-7 <abhaypratapsingh2993@gmail.com>
2026-01-12 02:25:19 +0530

    Sum Multiples

---
+++ leetcode/Sum_Multiples.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int sumOfMultiples(int n) {
+        int sum = 0;
+        for ( int i = 3; i <= n; i++ ){
+            if ( i % 3 == 0 || i % 5 == 0 || i % 7 == 0 ) sum += i;
+        }
+        return sum;
+    }
+
+int main()
+{
+    int n = 20;
+    cout << sumOfMultiples(n);
+    return 0;
+}
