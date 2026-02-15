fff2a6dbdac16cdba606e43354aec35ac2f6cda7
Abhay Pratap Singh <abhaypratapsingh2993@gmail.com>
2026-02-15 22:11:25 +0530

    Time: 3 ms (22.17%) | Memory: 12.5 MB (5.65%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int sum = 0;
+        int carry = 0;
+        int x;
+        int y;
+        string result = "";
+        while ( i >= 0 && j >= 0 ) {
+            x = (int)a[i] - (int)'0';
+            y = (int)b[j] - (int)'0';
+            sum = x ^ y ^ carry;
+            carry = ( x & y ) | ( carry & ( x ^ y ) );
+            result = to_string(sum) + result;
+            i--;
+            j--;
+        }
+        while ( i >= 0 ) {
+            x = (int)a[i] - (int)'0';
+            sum = x ^ carry;
+            carry = x & carry;
+            result = to_string(sum) + result;
+            i--;
+        }
+        while ( j >= 0 ) {
+            y = (int)b[j] - (int)'0';
+            sum = y ^ carry;
+            carry = y & carry;
+            result = to_string(sum) + result;
+            j--;
+        }
+        while ( carry != 0 ) {
+            result = to_string(carry) + result;
+            carry -= 1;
+        }
+        return result;
+    }
+};
