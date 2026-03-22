806eb1016558ae63be53599b4215d201a678f1b2
Abhay Pratap Singh <abhaypratapsingh2993@gmail.com>
2026-03-22 23:04:08 +0530

    Time: 3 ms (16.17%) | Memory: 12.4 MB (7.57%) - LeetSync

---
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minCost(int n) {
+        vector<int> dp;
+        dp.push_back(n);
+        int i = 0;
+        int temp;
+        int result = 0;
+        while ( i < dp.size() ) {
+            if ( dp[i] == 1 ) {
+                i++;
+                result++;
+                continue;
+            }
+            temp = dp[i] / 2;
+            dp.push_back(temp);
+            dp.push_back(dp[i]-temp);
+            result += temp * ( dp[i] - temp ); 
+            i++;
+        }
+        return result - n;
+    }
+};
